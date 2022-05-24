a4fbdb2e9c8dd876d60077fd24aa01766bddd379
Dominic ODonnell <dominicodonnell99@gmail.com>
2022-05-24 15:12:14 -0500

    Finishing touches on SCSS to reduce file size. Created nav hamburger for mobile use

--- static/javascript/nav.js
+++ static/javascript/nav.js
@@ -3,19 +3,34 @@
 
 let lastScrollTop = 0;
 const navbar = document.getElementById("navbar");
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const hamburgerContainer = document.getElementById("hamburger-container")
+
+// Adaptive navbar
 window.addEventListener("scroll", () => {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
 
+    // Hides navbar + hamburger on scroll
     if (scrollTop > lastScrollTop){
         navbar.style.top="-3rem";
+        hamburgerContainer.style.right="-7rem";
+    // Brings navbar into view on scroll-up, hides hamburger
     } else {
         navbar.style.top="0";
+        hamburgerContainer.style.right="-7rem";
     }
     lastScrollTop = scrollTop;
 })
 
+// Shows navbar on mouseover
 navbar.addEventListener("mouseover", () => {
     if (navbar.style.top="-3rem") {
         navbar.style.top="0";
     }
+})
+
+// Mobile hamburger
+// Shows hamburger on click of hamburger-icon
+hamburgerIcon.addEventListener("click", () => {
+    hamburgerContainer.style.right="0rem";
 })
