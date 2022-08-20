349ce38953243325bdb16de6b58a6d8b6f5f8cc5
dominicod <dominicodonnell99@gmail.com>
2022-08-20 14:46:38 -0500

    fix: move webApp import to top of file for convention

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,9 @@
 import os
 import sys
 import logging
+from webApp import app as application
+
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, "/var/www/html/dominwrites.com/")
-
-from webApp import app as application
 
 application.secret_key = '#'
